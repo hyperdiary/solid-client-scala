a4ccfd65ed407a464771623affd0a7f201f118fd
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2024-02-07 11:26:11 +0000

    Adding more methods and tests

--- src/test/scala/org/hyperdiary/SolidClientSuite.scala
+++ src/test/scala/org/hyperdiary/SolidClientSuite.scala
@@ -1,6 +1,6 @@
 package org.hyperdiary
 
-import sttp.model.{MediaType, StatusCode}
+import sttp.model.{ MediaType, StatusCode }
 
 class SolidClientSuite extends munit.FunSuite {
 
@@ -21,20 +21,69 @@ class SolidClientSuite extends munit.FunSuite {
       |]
       |""".stripMargin
 
-  test("create, retrieve and delete a named plain text resource") {
-    val resourceName = "myfile.txt"
-    val response1 = client.createResource(resourceName,textBody, MediaType.TextPlain)
+  private val patchedTurtleBody =
+    """<ex:s> <ex:p> <ex:o>.
+      |<ex:s2> <ex:p2> <ex:o2>.
+      |""".stripMargin
+
+  test("#1 create, retrieve and delete a named plain text resource") {
+    val resourceName = "text1.txt"
+    val response1 =
+      client.createResource(resourceName, textBody, MediaType.TextPlain)
     assertEquals(response1.code, StatusCode.Created)
-    val response2 = client.getResource(resourceName,"text/plain")
+
+    val response2 = client.getResource(resourceName, "text/plain")
     assertEquals(response2.code, StatusCode.Ok)
     assertEquals(response2.body, Right(textBody))
+
     val response3 = client.deleteResource(resourceName)
     assertEquals(response3.code, StatusCode.ResetContent)
   }
 
-  test("create, retrieve and delete a named turtle resource") {
-    val resourceName = "myfile.ttl"
-    val response1 = client.createResource(resourceName, turtleBody, MediaType("text","turtle"))
+  test("#2 create, retrieve headers and delete a named plain text resource") {
+    val resourceName = "test2.txt"
+    val response1 =
+      client.createResource(resourceName, textBody, MediaType.TextPlain)
+    assertEquals(response1.code, StatusCode.Created)
+
+    val response2 = client.getResourceHeaders(resourceName, "text/plain")
+    assertEquals(response2.code, StatusCode.Ok)
+    assertEquals(
+      response2.headers.find(_.name == "Content-Type").get.value,
+      "text/plain; charset=utf-8"
+    )
+
+    val response3 = client.deleteResource(resourceName)
+    assertEquals(response3.code, StatusCode.ResetContent)
+  }
+
+  test("#3 create, retrieve options and delete a named plain text resource") {
+    val resourceName = "test3.txt"
+    val response1 =
+      client.createResource(resourceName, textBody, MediaType.TextPlain)
+    assertEquals(response1.code, StatusCode.Created)
+
+    val response2 = client.getResourceOptions(resourceName)
+    assertEquals(response2.code, StatusCode.NoContent)
+    assertEquals(
+      response2.headers
+        .find(_.name == "Access-Control-Allow-Methods")
+        .get
+        .value,
+      "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE"
+    )
+
+    val response3 = client.deleteResource(resourceName)
+    assertEquals(response3.code, StatusCode.ResetContent)
+  }
+
+  test("#4 create, retrieve and delete a named turtle resource") {
+    val resourceName = "test4.ttl"
+    val response1 = client.createResource(
+      resourceName,
+      turtleBody,
+      MediaType("text", "turtle")
+    )
     assertEquals(response1.code, StatusCode.Created)
 
     val response2 = client.getResource(resourceName, "text/turtle")
@@ -49,26 +98,87 @@ class SolidClientSuite extends munit.FunSuite {
     assertEquals(response4.code, StatusCode.ResetContent)
   }
 
-  test("create, retrieve and delete a generated plain text resource") {
+  test("#5 create, retrieve and delete a generated plain text resource") {
     val response1 = client.createResource(textBody, MediaType.TextPlain)
     assertEquals(response1.code, StatusCode.Created)
-    val generatedResourceName = response1.headers.find(_.name == "Location").get.value.substring(hostname.length)
-    val response2 = client.getResource(generatedResourceName,"text/plain")
+
+    val generatedResourceName = response1.headers
+      .find(_.name == "Location")
+      .get
+      .value
+      .substring(hostname.length)
+    val response2 = client.getResource(generatedResourceName, "text/plain")
     assertEquals(response2.code, StatusCode.Ok)
     assertEquals(response2.body, Right(textBody))
+
     val response3 = client.deleteResource(generatedResourceName)
     assertEquals(response3.code, StatusCode.ResetContent)
   }
 
-  test("create, retrieve and delete a generated turtle resource") {
-    val response1 = client.createResource(turtleBody, MediaType("text","turtle"))
+  test("#6 create, retrieve and delete a generated turtle resource") {
+    val response1 =
+      client.createResource(turtleBody, MediaType("text", "turtle"))
     assertEquals(response1.code, StatusCode.Created)
-    val generatedResourceName = response1.headers.find(_.name == "Location").get.value.substring(hostname.length)
-    val response2 = client.getResource(generatedResourceName,"text/turtle")
+
+    val generatedResourceName = response1.headers
+      .find(_.name == "Location")
+      .get
+      .value
+      .substring(hostname.length)
+    val response2 = client.getResource(generatedResourceName, "text/turtle")
     assertEquals(response2.code, StatusCode.Ok)
     assertEquals(response2.body, Right(turtleBody))
+
     val response3 = client.deleteResource(generatedResourceName)
     assertEquals(response3.code, StatusCode.ResetContent)
+  }
+
+  test("#7 create, patch and delete a resource using N3 Patch") {
+    val resourceName = "test7.ttl"
+    val response1 = client.createResource(
+      resourceName,
+      turtleBody,
+      MediaType("text", "turtle")
+    )
+    assertEquals(response1.code, StatusCode.Created)
+
+    val patchBody =
+      "@prefix solid: <http://www.w3.org/ns/solid/terms#>. _:rename a solid:InsertDeletePatch; solid:inserts { <ex:s2> <ex:p2> <ex:o2>. }."
+    val response2 =
+      client.patchResource(resourceName, patchBody, MediaType("text", "n3"))
+    assertEquals(response2.code, StatusCode.ResetContent)
+
+    val response3 = client.getResource(resourceName, "text/turtle")
+    assertEquals(response3.code, StatusCode.Ok)
+    assertEquals(response3.body, Right(patchedTurtleBody))
+
+    val response4 = client.deleteResource(resourceName)
+    assertEquals(response4.code, StatusCode.ResetContent)
+  }
+
+  test("#8 create, patch and delete a resource using SPARQL Update") {
+    val resourceName = "test8.ttl"
+    val response1 = client.createResource(
+      resourceName,
+      turtleBody,
+      MediaType("text", "turtle")
+    )
+    assertEquals(response1.code, StatusCode.Created)
+
+    val patchBody = "INSERT DATA { <ex:s2> <ex:p2> <ex:o2> }"
+    val response2 = client.patchResource(
+      resourceName,
+      patchBody,
+      MediaType("application", "sparql-update")
+    )
+    assertEquals(response2.code, StatusCode.ResetContent)
+
+    val response3 = client.getResource(resourceName, "text/turtle")
+    assertEquals(response3.code, StatusCode.Ok)
+    assertEquals(response3.body, Right(patchedTurtleBody))
+
+    val response4 = client.deleteResource(resourceName)
+    assertEquals(response4.code, StatusCode.ResetContent)
   }
 
 }
