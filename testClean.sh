3706e5362e8d1d6a2cd2e9b9e10e4fe43f8b1c65
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2024-02-07 18:59:25 +0000

    Added a workflow test and refactored as necessary

--- testClean.sh
+++ testClean.sh
@@ -4,3 +4,5 @@ curl -X DELETE http://localhost:3000/test3.txt
 curl -X DELETE http://localhost:3000/test4.ttl
 curl -X DELETE http://localhost:3000/test7.ttl
 curl -X DELETE http://localhost:3000/test8.ttl
+curl -X DELETE http://localhost:3000/foo/
+curl -X DELETE http://localhost:3000/inbox/
