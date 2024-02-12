f799c6ba2165a2aa842e16b803801cff40a7b687
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2024-02-12 11:24:48 +0000

    More work on loading

--- src/main/scala/org/hyperdiary/solid/pod/PodLoader.scala
+++ src/main/scala/org/hyperdiary/solid/pod/PodLoader.scala
@@ -1,26 +1,29 @@
 package org.hyperdiary.solid.pod
 
+import org.apache.jena.rdf.model.{ Model, Resource }
+import org.apache.jena.riot.{ Lang, RDFDataMgr }
 import org.hyperdiary.solid.client.SolidClient
 import org.hyperdiary.solid.model.Label
 import sttp.client3.UriContext
-import sttp.model.{MediaType, Uri}
+import sttp.model.{ MediaType, Uri }
 
+import java.io.StringWriter
 import scala.io.Source
-import scala.util.{Failure, Success, Try, Using}
+import scala.jdk.CollectionConverters.*
+import scala.util.{ Failure, Success, Try, Using }
 
 class PodLoader(podUrl: String) {
 
   private val client = new SolidClient()
 
   // 1) create a container e.g. label/
-  def createContainers(containerNames: List[String]): Unit = {
+  def createContainers(containerNames: List[String]): Unit =
     containerNames.foreach(createContainer)
-  }
 
   // 2) create a generated resource with the container e.g. /label/41cf17c4-292d-4d85-b2cf-5513c005d424
   // 3) retrieve the generated URL from the Location header
   def generateResource(containerName: String): Option[String] = {
-    val response = client.postResource(uri"$podUrl$containerName/","",MediaType("text","turtle"))
+    val response = client.postResource(uri"$podUrl$containerName/", "", MediaType("text", "turtle"))
     response.body match {
       case Left(body) =>
         println(s"Non-2xx response to GET with code ${response.code}:\n$body")
@@ -30,8 +33,8 @@ class PodLoader(podUrl: String) {
         response.headers.find(_.name == "Location").flatMap(header => Some(header.value))
     }
   }
-  
-  def readLabelsCsv(labelCsvPath: String): List[Label] = {
+
+  def readLabelsCsv(labelCsvPath: String): List[Label] =
     readLines(labelCsvPath) match {
       case Success(lines) =>
         lines.map { line =>
@@ -42,32 +45,53 @@ class PodLoader(podUrl: String) {
         println(s"Failed to read file due to ${e.getMessage}")
         List.empty
     }
-  }
 
   def insertAsSparqlUpdate(resourceUri: Uri, resourceBody: String): Unit = {
     val sparqlInsert = s"INSERT DATA { $resourceBody }"
-    val response = client.patchResource(resourceUri,sparqlInsert, MediaType("application","sparql-update"))
+    val response = client.patchResource(resourceUri, sparqlInsert, MediaType("application", "sparql-update"))
     response.body match {
-      case Left(body) => println(s"Non-2xx response to GET with code ${response.code}:\n$body")
+      case Left(body)  => println(s"Non-2xx response to GET with code ${response.code}:\n$body")
       case Right(body) => println(s"2xx response to GET:\n$body")
     }
   }
 
+  def loadTurtleFile(turtlePath: String): Unit = {
+    val model = RDFDataMgr.loadModel(turtlePath)
+    model.listSubjects().toList.asScala.foreach { resource =>
+      load(resource)
+    }
+  }
+
+  def load(resource: Resource): Unit = {
+    val uri = resource.getURI.substring(podUrl.length - 1).split("/")
+    val resourceModel = resource.listProperties().toModel
+    val response =
+      client.putResource(uri"$podUrl/${uri(0)}/${uri(1)}", modelAsString(resourceModel), MediaType("text", "turtle"))
+    response.body match {
+      case Left(body)  => println(s"Non-2xx response to GET with code ${response.code}:\n$body")
+      case Right(body) => println(s"2xx response to GET:\n$body")
+    }
+  }
+
+  private def modelAsString(model: Model) = {
+    val out = new StringWriter()
+    RDFDataMgr.write(out, model.listStatements().toModel, Lang.TTL)
+    out.toString
+  }
+
   // 4) read labels from CSV file
-  private def readLines(labelCsvPath: String): Try[List[String]] = {
+  private def readLines(labelCsvPath: String): Try[List[String]] =
     Using(Source.fromFile(labelCsvPath)) { bufferedSource =>
       (for (line <- bufferedSource.getLines()) yield line).toList
     }
-  }
-  
-  
+
   // 5) for each label create an RDF resource with type
   // 6) send a sparql-update request to add the resource triples by serializing the RDF resource
 
   private def createContainer(containerName: String): Unit = {
     val response = client.putContainer(uri"$podUrl$containerName/")
     response.body match {
-      case Left(body) => println(s"Non-2xx response to GET with code ${response.code}:\n$body")
+      case Left(body)  => println(s"Non-2xx response to GET with code ${response.code}:\n$body")
       case Right(body) => println(s"2xx response to GET:\n$body")
     }
   }
--- src/test/scala/org/hyperdiary/solid/pod/PodLoaderSuite.scala
+++ src/test/scala/org/hyperdiary/solid/pod/PodLoaderSuite.scala
@@ -4,6 +4,7 @@ import org.apache.jena.rdf.model.ModelFactory
 import org.apache.jena.riot.{Lang, RDFDataMgr}
 import sttp.client3.UriContext
 import sttp.model.Uri
+import scala.jdk.CollectionConverters.*
 
 import java.io.StringWriter
 
@@ -17,20 +18,39 @@ class PodLoaderSuite extends munit.FunSuite {
 //    } yield println(resource)
   }
 
-  test("test2") {
+//  test("test2") {
+//    val hostname = "http://krw.localhost:3000/"
+//    val collectionName = "label"
+//    val loader = new PodLoader(hostname)
+//    val labelFilePath = getClass.getResource("/labels.csv").getPath
+//    val labels = loader.readLabelsCsv(labelFilePath: String)
+//    val rdfLabels: List[(Uri,String)] = labels.flatMap { label =>
+//      val res = loader.generateResource(collectionName)
+//      res.flatMap(uri =>
+//        val id = uri.substring(uri.lastIndexOf('/') + 1)
+//        Some((uri"$hostname$collectionName/$id", label.asRdfString(uri))))
+//    }
+//    rdfLabels.foreach(rdfLabel => loader.insertAsSparqlUpdate(rdfLabel._1,rdfLabel._2))
+//    assertEquals(rdfLabels.length, 7)
+//  }
+
+  test("test3") {
     val hostname = "http://krw.localhost:3000/"
-    val collectionName = "label"
     val loader = new PodLoader(hostname)
-    val labelFilePath = getClass.getResource("/labels.csv").getPath
-    val labels = loader.readLabelsCsv(labelFilePath: String)
-    val rdfLabels: List[(Uri,String)] = labels.flatMap { label =>
-      val res = loader.generateResource(collectionName)
-      res.flatMap(uri =>
-        val id = uri.substring(uri.lastIndexOf('/') + 1)
-        Some((uri"$hostname$collectionName/$id", label.asRdfString(uri))))
-    }
-    rdfLabels.foreach(rdfLabel => loader.insertAsSparqlUpdate(rdfLabel._1,rdfLabel._2))
-    assertEquals(rdfLabels.length, 7)
+    val entriesFilePath = getClass.getResource("/krw-entries.ttl").getPath
+    loader.loadTurtleFile(entriesFilePath)
+    val journalsFilePath = getClass.getResource("/krw-journals.ttl").getPath
+    loader.loadTurtleFile(journalsFilePath)
+    val labelsFilePath = getClass.getResource("/krw-labels.ttl").getPath
+    loader.loadTurtleFile(labelsFilePath)
+    val peopleFilePath = getClass.getResource("/krw-people.ttl").getPath
+    loader.loadTurtleFile(peopleFilePath)
+    val placesFilePath = getClass.getResource("/krw-places.ttl").getPath
+    loader.loadTurtleFile(placesFilePath)
+    val residencesFilePath = getClass.getResource("/krw-residences.ttl").getPath
+    loader.loadTurtleFile(residencesFilePath)
+    val thingsFilePath = getClass.getResource("/krw-things.ttl").getPath
+    loader.loadTurtleFile(thingsFilePath)
   }
 
 }
