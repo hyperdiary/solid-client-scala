cff8bfabaa4f7c62fe606f82e09148284f052949
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2024-03-09 16:18:47 +0000

    Update config filename

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,1 +1,1 @@
-docker run --name solid_journal --rm -v ~/Solid:/data -v ~/solid-config:/config  -p 3000:3000 -it solidproject/community-server:latest -c /config/my-config.json
+docker run --name solid_journal --rm -v ~/Solid:/data -v ~/solid-config:/config  -p 3000:3000 -it solidproject/community-server:latest -c /config/css-config.json
