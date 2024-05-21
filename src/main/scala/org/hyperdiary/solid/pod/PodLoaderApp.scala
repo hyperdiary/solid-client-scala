fd761b569cf4f601bf4f14a258bdf01a759231b3
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2024-05-21 11:41:33 +0100

    WIP

--- src/main/scala/org/hyperdiary/solid/pod/PodLoaderApp.scala
+++ src/main/scala/org/hyperdiary/solid/pod/PodLoaderApp.scala
@@ -1,17 +1,20 @@
 package org.hyperdiary.solid.pod
 
+import cats.implicits.*
 import com.monovore.decline.{CommandApp, Opts}
 
 object PodLoaderApp extends CommandApp(
       name = "Solid Pod Loader",
       header = "Loads data into a Solid Pod",
       main = {
-        val inputPath =
+        val inputFile =
           Opts.option[String]("file", short = "f", help = "File to load")
 
-        inputPath.map { path =>
-          val loader = new PodLoader("http://krw.localhost:3000/")
-          loader.loadTurtleFile(path)
+        val podUrl = Opts.option[String]("pod", short = "p", help = "Pod URL")
+
+        (inputFile,podUrl).mapN { (file,pod) =>
+          val loader = new PodLoader(pod)
+          loader.loadTurtleFile(file)
         }
       }
     )
